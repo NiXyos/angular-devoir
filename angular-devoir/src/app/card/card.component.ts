bcc858404356ace1ec006b4d891f524226718d53
NiXyos <54945554+NiXyos@users.noreply.github.com>
2022-03-02 17:38:45 +0100

    0.4 (Problème Réglé)

--- angular-devoir/src/app/card/card.component.ts
+++ angular-devoir/src/app/card/card.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Film } from '../types';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() film!: Film;
 
   constructor() { }
 
--- angular-devoir/src/app/main-center/main-center.component.ts
+++ angular-devoir/src/app/main-center/main-center.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Film } from '../types';
+
 @Component({
   selector: 'app-main-center',
   templateUrl: './main-center.component.html',
   styleUrls: ['./main-center.component.css']
 })
 export class MainCenterComponent implements OnInit {
+
+
+  films: Film[] = [
+    {
+    picture: "https://image.tmdb.org/t/p/w780//1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+    title: "Harry potter"
+    }
+  ];
 
   constructor() { }
 
