4c547a0f7b567fc29992ad78c7f903431e0b6218
NiXyos <54945554+NiXyos@users.noreply.github.com>
2022-03-02 13:50:48 +0100

    master

--- angular-devoir/src/app/app.module.ts
+++ angular-devoir/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { CenterComponent } from './center/center.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarMenuComponent,
+    CenterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
